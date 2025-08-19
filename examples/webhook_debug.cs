760ac033d6dbe2f72fac0dfd6ca1d3fa47139b35
joel-maws <joel@mobileaws.com>
2025-08-18 17:52:29 -0700

    cleanup env

--- examples/webhook_debug.cs
+++ examples/webhook_debug.cs
@@ -12,8 +12,8 @@ public class WebhookDebug
     public static async Task RunAsync(string ngrokUrl)
     {
         // API credentials
-        var clientId = "2682";
-        var apiKey = "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJpbmZvQGFsbGNvZGUuY29tIiwiaXNzIjoiY2xvdWRjb250YWN0IiwibmJmIjoxNzE5NDQwMjM2LCJpYXQiOjE3MTk0NDAyMzYsInJvbGUiOiJVU0VSIiwiY2xpZW50SWQiOjI2ODIsImlkIjoyNzY0LCJ0eXBlIjoiQVBJX0tFWSIsImtleV9yYW5kb21faWQiOiI1MGRiOTUzZC1hMjUxLTRmZjMtODI5Yi01NjIyOGRhOGE1YTAifQ.PKVjXYHdjBMum9cTgLzFeY2KIb9b2tjawJ0WXalsb8Bckw1RuxeiYKS1bw5Cc36_Rfmivze0T7r-Zy0PVj2omDLq65io0zkBzIEJRNGDn3gx_AqmBrJ3yGnz9s0WTMr2-F1TFPUByzbj1eSOASIKeI7DGufTA5LDrRclVkz32Oo";
+        var clientId = "YOUR_CLIENT_ID";
+        var apiKey = "YOUR_API_KEY";
         
         // Create HTTP client
         using var httpClient = new HttpClient();
--- examples/webhook_register.cs
+++ examples/webhook_register.cs
@@ -19,8 +19,8 @@ public class WebhookRegister
         
         var config = new CCAIConfig
         {
-            ClientId = "2682",
-            ApiKey = "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJpbmZvQGFsbGNvZGUuY29tIiwiaXNzIjoiY2xvdWRjb250YWN0IiwibmJmIjoxNzE5NDQwMjM2LCJpYXQiOjE3MTk0NDAyMzYsInJvbGUiOiJVU0VSIiwiY2xpZW50SWQiOjI2ODIsImlkIjoyNzY0LCJ0eXBlIjoiQVBJX0tFWSIsImtleV9yYW5kb21faWQiOiI1MGRiOTUzZC1hMjUxLTRmZjMtODI5Yi01NjIyOGRhOGE1YTAifQ.PKVjXYHdjBMum9cTgLzFeY2KIb9b2tjawJ0WXalsb8Bckw1RuxeiYKS1bw5Cc36_Rfmivze0T7r-Zy0PVj2omDLq65io0zkBzIEJRNGDn3gx_AqmBrJ3yGnz9s0WTMr2-F1TFPUByzbj1eSOASIKeI7DGufTA5LDrRclVkz32Oo"
+            ClientId = "YOUR_CLIENT_ID",
+            ApiKey = "YOUR_API_KEY"
         };
         
         using var ccai = new CCAIClient(config);
